283193d992bb61cb1e870873cec0a6798e66783e
anderrf <anderson_rocha_farias@outlook.com>
2023-02-09 16:35:36 -0300

    Basic route functions

---
+++ src/models/task.js
@@ -0,0 +1,46 @@
+import { randomUUID } from 'node:crypto';
+
+export class Task{
+    id;
+    title;
+    description;
+    completed_at;
+    created_at;
+    updated_at;
+
+    constructor(taskData){
+        let currentDate = new Date();
+        if(taskData){
+            this.id = taskData.id ? taskData.id : randomUUID();
+            this.title = taskData.title;
+            this.description = taskData.description;
+            this.created_at = taskData.created_at ? taskData.created_at : currentDate;
+            this.completed_at = taskData.completed_at ? taskData.completed_at : null;
+            this.updated_at = taskData.updated_at ? taskData.updated_at : currentDate;
+        }
+        else{
+            this.id = randomUUID();
+            this.created_at = currentDate;
+            this.completed_at = null;
+            this.updateTask = currentDate;
+        }
+    }
+
+    static createTaskFromData(taskData){
+        return new Task(taskData);
+    }
+
+    markAsCompleted(){
+        let currentDate = new Date();
+        this.completed_at = currentDate;
+        this.updated_at = currentDate;
+    }
+
+    updateTask(updatedData){
+        let currentDate = new Date();
+        this.title = updatedData.title ?? '';
+        this.description = updatedData.description ?? '';
+        this.completed_at = updatedData.completed_at ?? null;
+        this.updated_at = currentDate;
+    }
+}
--- src/routes.js
+++ src/routes.js
@@ -1,37 +1,97 @@
+import { buildRoutePath } from './utils/build-route-path.js';
+import { randomUUID } from 'node:crypto';
+import { Task } from './models/task.js';
+
+const allTasks = [];
+
 export const routes = [
     {
         method: 'POST',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (request, response) => {
-
+            let task = Task.createTaskFromData(request.body);
+            allTasks.push(task);
+            return response
+                .setHeader('Content-type', 'application/json')
+                .writeHead(201)
+                .end(JSON.stringify(task));
         }
     },
     {
         method: 'GET',
-        path: '/tasks',
+        path: buildRoutePath('/tasks'),
         handler: (request, response) => {
-            
+            return response
+                .setHeader('Content-type', 'application/json')
+                .writeHead(200)
+                .end(JSON.stringify([...allTasks]));
         }
     },
     {
         method: 'PUT',
-        path: '/tasks/:id',
+        path: buildRoutePath('/tasks/:id'),
         handler: (request, response) => {
-            
+            const {id} = request.params;
+            let rowIndex = allTasks.findIndex(task => {
+                return task.id === id;
+            });
+            if(rowIndex > -1){
+                let taskToUpdate = Task.createTaskFromData(allTasks[rowIndex]);
+                taskToUpdate.updateTask(request.body);
+                allTasks[rowIndex] = taskToUpdate;
+                return response
+                    .setHeader('Content-type', 'application/json')
+                    .writeHead(200)
+                    .end(JSON.stringify(taskToUpdate));
+            }
+            return response
+                .setHeader('Content-type', 'application/json')
+                .writeHead(404)
+                .end("Task not found!");
         }
     },
     {
         method: 'DELETE',
-        path: '/tasks/:id',
+        path: buildRoutePath('/tasks/:id'),
         handler: (request, response) => {
-            
+            const {id} = request.params;
+            let rowIndex = allTasks.findIndex(task => {
+                return task.id === id;
+            });
+            if(rowIndex > -1){
+                let deletedTask = allTasks.splice(rowIndex, 1);
+                return response
+                    .setHeader('Content-type', 'application/json')
+                    .writeHead(204)
+                    .end(JSON.stringify(deletedTask));
+            }
+            return response
+                .setHeader('Content-type', 'application/json')
+                .writeHead(404)
+                .end("Task not found!");
         }
     },
     {
         method: 'PATCH',
-        path: '/tasks/:id/complete',
+        path: buildRoutePath('/tasks/:id/complete'),
         handler: (request, response) => {
-            
+            const {id} = request.params;
+            let rowIndex = allTasks.findIndex(task => {
+                return task.id === id;
+            });
+            if(rowIndex > -1){
+                let completedTask = Task.createTaskFromData(allTasks[rowIndex]);
+                completedTask.markAsCompleted();
+                allTasks[rowIndex] = completedTask;
+                return response
+                    .setHeader('Content-type', 'application/json')
+                    .writeHead(200)
+                    .end(JSON.stringify(completedTask));
+            }
+            return response
+                .setHeader('Content-type', 'application/json')
+                .writeHead(404)
+                .end("Task not found!");
         }
     }
 ];
